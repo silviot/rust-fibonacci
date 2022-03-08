83e27d23b1f0f70732f36e2c3bff1e981a64fb2b
Silvio Tomatis <silviot@gmail.com>
2022-03-08 16:41:12 +0100

    Add cache to fibonacci

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-fn fibonacci(n: u64) -> u64 {
+fn fibonacci_memoized(n: u64, previous_results: &mut Vec<u64>) -> u64 {
+    if previous_results.len() >= (n + 1).try_into().unwrap() {
+        return previous_results[n as usize];
+    }
     if n == 0 || n == 1 {
         return 1;
     }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    let result = fibonacci_memoized(n - 1, previous_results) + fibonacci_memoized(n - 2, previous_results);
+    if previous_results.len() == n as usize {
+        previous_results.push(result);
+    }
+    result
+}
+
+
+fn fibonacci(n: u64) -> u64 {
+    let mut previous_results = vec![1u64, 1];
+    return fibonacci_memoized(n, &mut previous_results);
 }
 
 fn main() {
@@ -26,5 +39,12 @@ fn slow_cases() {
     assert_eq!(fibonacci(20), 10946);
     assert_eq!(fibonacci(30), 1346269);
     assert_eq!(fibonacci(40), 165580141);
-    assert_eq!(fibonacci(50), 12586269025);
+    assert_eq!(fibonacci(49), 12586269025);
+    assert_eq!(fibonacci(60), 2504730781961);
+    assert_eq!(fibonacci(70), 308061521170129);
+    assert_eq!(fibonacci(80), 37889062373143906);
+    assert_eq!(fibonacci(90), 4660046610375530309);
+    assert_eq!(fibonacci(92), 12200160415121876738);
+    // This one is too big for u64
+    // assert_eq!(fibonacci(93), ????);
 }
